4b80cae7cda377707bc9bde2642b198bc6cadb7c
Mel Rumsey <melsrumsey@gmail.com>
2020-08-31 15:05:57 -0500

    Initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void MovePlayer() 
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");  
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0 , verticalInput);
+        Vector3 velocity = _speed * direction;
+        velocity.y = _yVelocity;
+        _yVelocity -= _gravity; 
+
+        velocity = transform.transform.TransformDirection(velocity);
+
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
